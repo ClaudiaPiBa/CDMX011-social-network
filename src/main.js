dca85a30397c516b9211b64241994cc9d70a84ee
Zeroba <zero.pi.ba@gmail.com>
2021-10-05 11:31:49 -0500

    Agregamos estilo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
 // Este es el punto de entrada de tu aplicacion
 
+import { Home } from './components/Home.js';
+import { Register } from './components/Register.js';
+import { Login } from './components/Login.js';
 import { myFunction } from './lib/index.js';
 
-myFunction();
+myFunction()
+const rootDiv = document.getElementById('root');
+
+const routes = {
+    '/': Login,
+    '/register': Register,
+    '/home' : Home
+};
+
+export const onNavigate = (pathname) => {
+  window.history.pushState(
+      {},
+      pathname,
+      window.location.origin + pathname
+  );
+
+  while(rootDiv.firstChild) {
+      rootDiv.removeChild(rootDiv.firstChild);
+  }
+  rootDiv.append(routes[pathname]());
+};
+const component = routes[window.location.pathname];
+
+window.onpopstate = () => {
+    rootDiv.append(component());
+}
+
+rootDiv.append(component());
